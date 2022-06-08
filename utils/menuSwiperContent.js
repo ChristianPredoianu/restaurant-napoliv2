494d6b013313cf3ff65eff6cec0bfbdd15651827
Christian Predoianu <christianpredoianu@yahoo.com>
2022-06-08 15:36:29 +0200

    added utils folder

---
+++ utils/menuSwiperContent.js
@@ -0,0 +1,21 @@
+import PizzaImg from '@/assets/images/pizza.jpg';
+import HamburgerImg from '@/assets/images/hamburger.jpg';
+import KebabImg from '@/assets/images/kebab.jpg';
+import SaladImg from '@/assets/images/pasta.jpg';
+import PastaImg from '@/assets/images/salad.jpg';
+import SteakImg from '@/assets/images/steak.jpg';
+import PlankImg from '@/assets/images/plank.jpg';
+import PotatoeImg from '@/assets/images/potatoe.jpg';
+import FishImg from '@/assets/images/fish.jpg';
+
+export const menuSwiperContent = [
+  { id: 1, img: PizzaImg, text: 'Pizza' },
+  { id: 2, img: HamburgerImg, text: 'Hamburgare' },
+  { id: 3, img: SteakImg, text: 'Kött' },
+  { id: 4, img: SaladImg, text: 'Sallad' },
+  { id: 5, img: PastaImg, text: 'Pasta' },
+  { id: 6, img: KebabImg, text: 'Kebab' },
+  { id: 7, img: PlankImg, text: 'Planka' },
+  { id: 8, img: PotatoeImg, text: 'Potatis' },
+  { id: 9, img: FishImg, text: 'Fisk' },
+];
---
+++ utils/verticalSwiperImages.js
@@ -0,0 +1,21 @@
+import PizzaImg from '@/assets/images/pizza1.png';
+import HamburgerImg from '@/assets/images/hamburger1.png';
+import KebabImg from '@/assets/images/kebab1.png';
+import WineImg from '@/assets/images/wine1.png';
+import Salad1Img from '@/assets/images/salad1.png';
+import Salad2Img from '@/assets/images/salad2.png';
+import SteakImg from '@/assets/images/steak1.png';
+import SodaImg from '@/assets/images/soda1.png';
+import BeerImg from '@/assets/images/beer1.png';
+
+export const verticalSwiperImages = [
+  { id: 1, img: PizzaImg },
+  { id: 2, img: HamburgerImg },
+  { id: 3, img: SteakImg },
+  { id: 4, img: Salad1Img },
+  { id: 5, img: WineImg },
+  { id: 6, img: KebabImg },
+  { id: 7, img: SodaImg },
+  { id: 8, img: BeerImg },
+  { id: 9, img: Salad2Img },
+];
