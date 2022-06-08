4a1103be7a8b977378f4e77ad44734746f5dc5ef
Christian Predoianu <christianpredoianu@yahoo.com>
2022-06-08 15:40:18 +0200

    added custom

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,15 +10,28 @@ module.exports = {
         'white-landing': 'url(../assets/images/bg-image.jpg)',
       },
       colors: {
-        'hero-overlay': 'rgba(0, 0, 0, .4)',
+        'hero-overlay': 'rgba(0, 0, 0, .6)',
         'hero-overlay-dark': 'rgba(0, 0, 0, .6)',
         'dark-mode-blue': '#121212',
       },
       dropShadow: {
         '3xl': '0 0 5px rgba(209, 114, 19, 1)',
       },
+      boxShadow: {
+        '3xl': '1px 2px 10px 5px rgba(61,61,61,0.73)',
+      },
       spacing: {
         '10rem': '28rem',
+      },
+      keyframes: {
+        typing: {
+          from: '{ width: 0 }',
+          to: '{ width: 100% }',
+        },
+        blinking: {
+          from: '{ border-color: transparent }',
+          to: '{ border-color: green; }',
+        },
       },
       height: {
         128: '50rem',
