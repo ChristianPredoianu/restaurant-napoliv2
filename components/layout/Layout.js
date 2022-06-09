8120cd8bc1bc84d4c3d3aeadeca27675f5c0740e
Christian Predoianu <christianpredoianu@yahoo.com>
2022-06-09 14:42:09 +0200

    added scrollTopComponent

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,10 +1,13 @@
 import Nav from '@/components/nav/Nav';
+import ScrollToTop from '@/components/ui/ScrollToTop';
 
 export default function Layout({ children }) {
   return (
     <>
       <Nav />
       <main>{children}</main>
+
+      <ScrollToTop />
     </>
   );
 }
