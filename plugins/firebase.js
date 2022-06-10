9a71e6575b2841c06ef97b971a6bdd19954579ef
Christian Predoianu <christianpredoianu@yahoo.com>
2022-06-10 11:04:09 +0200

    added firebase

---
+++ plugins/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBX6hgm1Tr4U_FxYP65Hj7aynQkEOMr74U',
+  authDomain: 'napoliv2.firebaseapp.com',
+  projectId: 'napoliv2',
+  storageBucket: 'napoliv2.appspot.com',
+  messagingSenderId: '450778939645',
+  appId: '1:450778939645:web:81d2ec8264d00ffef4be94',
+};
+
+const app = initializeApp(firebaseConfig);
