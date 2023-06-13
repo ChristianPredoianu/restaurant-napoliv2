6d11fb7a9cff434eac942c185c577df503c63ece
Christian Predoianu <christianpredoianu@yahoo.com>
2023-06-13 09:48:33 +0200

    changed link

--- components/footer/Footer.js
+++ components/footer/Footer.js
@@ -7,53 +7,51 @@ import GMap from '@/components/ui/GMap';
 
 export default function Footer() {
   return (
-    <footer className="bg-dark-mode-blue py-20 dark:border-t border-amber-600">
-      <div className="container mx-auto w-24 pb-10">
+    <footer className='bg-dark-mode-blue py-20 dark:border-t border-amber-600'>
+      <div className='container mx-auto w-24 pb-10'>
         <NavLogo />
       </div>
       <GMap />
-      <div className="container mx-auto border-b border-amber-600 mt-10"></div>
-      <div className="text-gray-200 text-2xl container mx-auto flex justify-center items-center mt-16">
+      <div className='container mx-auto border-b border-amber-600 mt-10'></div>
+      <div className='text-gray-200 text-2xl container mx-auto flex justify-center items-center mt-16'>
         <a
-          href="https://www.facebook.com/NapoliRestaurang"
-          rel="noreferrer nofollow"
-          target="_blank"
-          className="hover:scale-105 hover:text-amber-600"
+          href='https://www.facebook.com/NapoliRestaurang'
+          rel='noreferrer nofollow'
+          target='_blank'
+          className='hover:scale-105 hover:text-amber-600'
         >
           <FontAwesomeIcon
             icon={faFacebook}
-            className="cursor-pointer transition duration-150 ease-in-out"
+            className='cursor-pointer transition duration-150 ease-in-out'
           />
         </a>
         <a
-          href="https://www.instagram.com/napoli_restaurang/"
-          rel="noreferrer nofollow"
-          target="_blank"
-          className="hover:scale-105 hover:text-amber-600"
+          href='https://www.instagram.com/napoli_restaurang/'
+          rel='noreferrer nofollow'
+          target='_blank'
+          className='hover:scale-105 hover:text-amber-600'
         >
           <FontAwesomeIcon
             icon={faInstagram}
-            className="ml-5 cursor-pointer transition duration-150 ease-in-out"
+            className='ml-5 cursor-pointer transition duration-150 ease-in-out'
           />
         </a>
         <a
-          href="mailto:musti_076@napoli.com"
-          className="hover:scale-105 hover:text-amber-600"
+          href='mailto:musti_076@napoli.com'
+          className='hover:scale-105 hover:text-amber-600'
         >
           <FontAwesomeIcon
             icon={faEnvelope}
-            className="ml-5 cursor-pointer transition duration-150 ease-in-out"
+            className='ml-5 cursor-pointer transition duration-150 ease-in-out'
           />
         </a>
       </div>
       <a
-        href="https://www.christianpredoianu.se"
-        rel="noreferrer nofollow"
-        target="_blank"
+        href='https://github.com/ChristianPredoianu'
+        rel='noreferrer nofollow'
+        target='_blank'
       >
-        <p className="text-amber-600 text-center mt-10">
-          &copy; Christian Predoianu
-        </p>
+        <p className='text-amber-600 text-center mt-10'>&copy; Christian Predoianu</p>
       </a>
     </footer>
   );
