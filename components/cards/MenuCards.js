5fc3ce7186e6cdb909ee13d018bf7d5797578d96
Christian Predoianu <christianpredoianu@yahoo.com>
2023-06-13 10:04:38 +0200

    changed prices

--- components/cards/MenuCards.js
+++ components/cards/MenuCards.js
@@ -6,59 +6,48 @@ import data from '../../data.json';
 
 export default forwardRef(function MenuCards({}, ref) {
   const menuCard1 = (
-    <MenuCard
-      heading={'Pizzor'}
-      subHeading={'Tomat & ost ingår på alla pizzor'}
-    >
+    <MenuCard heading={'Pizzor'} subHeading={'Tomat & ost ingår på alla pizzor'}>
       <MenuList
         data={data.meals.priceclass1}
-        heading={'Prisklass 1/90:- Familjepizza 220:-'}
+        heading={'Prisklass 1/100:- Familjepizza 250:-'}
         ref={ref.current.pizzaRef}
       />
       <MenuList
         data={data.meals.priceclass2}
-        heading={'Prisklass 2/95:- Familjepizza 230:-'}
+        heading={'Prisklass 2/105:- Familjepizza 260:-'}
       />
       <MenuList
         data={data.meals.priceclass3}
-        heading={'Prisklass 3/100:- Familjepizza 240:-'}
+        heading={'Prisklass 3/110:- Familjepizza 270:-'}
       />
       <MenuList
         data={data.meals.priceclass4}
-        heading={'Prisklass 4/110:- Familjepizza 270:-'}
+        heading={'Prisklass 4/120:- Familjepizza 280:-'}
       />
     </MenuCard>
   );
 
   const menuCard2 = (
-    <MenuCard
-      heading={'Pizzor'}
-      subHeading={'Tomat & ost ingår på alla pizzor'}
-    >
+    <MenuCard heading={'Pizzor'} subHeading={'Tomat & ost ingår på alla pizzor'}>
       <MenuList data={data.meals.kebabPizzas} category={'Kebabpizzor'} />
       <MenuList
         data={data.meals.specialPizzas}
-        heading={'Pris 130:- Familjepizza 300:- Med mozzarellaost'}
+        heading={'Pris 140:- Familjepizza 300:- Med mozzarellaost'}
         category={'Specialpizzor'}
       />
       <MenuList
         data={data.meals.plankPizzas}
-        heading={'Pris 120:- Familjepizza 300:-'}
+        heading={'Pris 130:- Familjepizza 300:-'}
         category={'Plankpizzor'}
       />
       <MenuList
         data={data.meals.americanPanPizzas}
-        heading={
-          'Small 120:- 1 pers. Medium 160:- 1-2 pers. Large 300:- 2-3 pers'
-        }
+        heading={'Small 120:- 1 pers. Medium 160:- 1-2 pers. Large 300:- 2-3 pers'}
         category={'American Pan Pizza'}
       />
-      <MenuList
-        data={data.meals.italianPizzas}
-        category={'Italienska Pizzor'}
-      />
-      <div className="block w-full mt-20 lg:mt-40">
-        <img src={GirlPizzaImg.src} alt="girl eating pizza" />
+      <MenuList data={data.meals.italianPizzas} category={'Napolitanska Pizzor'} />
+      <div className='block w-full mt-20 lg:mt-40'>
+        <img src={GirlPizzaImg.src} alt='girl eating pizza' />
       </div>
     </MenuCard>
   );
@@ -68,17 +57,17 @@ export default forwardRef(function MenuCards({}, ref) {
       <MenuList
         data={data.meals.kebabChicken}
         heading={'Tomat, lök och gurka ingår.'}
-        category={'Kebab, kyckling, falafel & taco 105:-'}
+        category={'Kebab, kyckling, falafel & taco 115:-'}
         ref={ref.current.kebabRef}
       />
       <MenuList
         data={data.meals.salads}
-        category={'Sallader 105:-'}
+        category={'Sallader 115:-'}
         ref={ref.current.saladRef}
       />
       <MenuList
         data={data.meals.bakedPotatoes}
-        category={'Bakad Potatis 105:-'}
+        category={'Bakad Potatis 115:-'}
         ref={ref.current.potatoesRef}
       />
       <MenuList
@@ -86,23 +75,15 @@ export default forwardRef(function MenuCards({}, ref) {
         category={'Hamburgare'}
         ref={ref.current.hamburgerRef}
       />
-      <MenuList
-        data={data.meals.pasta}
-        category={'Pasta'}
-        ref={ref.current.pastaRef}
-      />
-      <MenuList
-        data={data.meals.plank}
-        category={'Planka'}
-        ref={ref.current.plankRef}
-      />
+      <MenuList data={data.meals.pasta} category={'Pasta'} ref={ref.current.pastaRef} />
+      <MenuList data={data.meals.plank} category={'Planka'} ref={ref.current.plankRef} />
       <MenuList
         data={data.meals.meatFish}
         category={'Kött & Fiskrätter'}
         heading={'Välj mellan kokt, stekt potatis eller pommes.'}
         ref={ref.current.meatRef}
       />
-      <div className="block w-full mt-20 lg:mt-40"></div>
+      <div className='block w-full mt-20 lg:mt-40'></div>
     </MenuCard>
   );
 
