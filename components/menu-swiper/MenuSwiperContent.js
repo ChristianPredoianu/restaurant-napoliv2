5df407475d0156d95c330c488a79416473664327
Christian Predoianu <christianpredoianu@yahoo.com>
2024-05-23 15:49:46 +0200

    work

--- components/menu-swiper/MenuSwiperContent.js
+++ components/menu-swiper/MenuSwiperContent.js
@@ -7,13 +7,13 @@ export default function MenuSwiperContent({ item, onScrollToSection }) {
 
   return (
     <div
-      className="block w-full bg-cover bg-center h-full relative  transition duration-300 hover:opacity-80 cursor-pointer"
+      className='block w-full bg-cover bg-center h-full relative  transition duration-300 hover:opacity-80 cursor-pointer'
       onClick={scrollToSectionHandler}
     >
-      <div className="absolute inset-0 bg-hero-overlay z-20 flex items-center justify-center ">
-        <p className="text-gray-200 text-3xl tracking-widest">{text}</p>
+      <div className='absolute inset-0 bg-hero-overlay z-20 flex items-center justify-center '>
+        <p className='text-gray-200 text-3xl tracking-widest'>{text}</p>
       </div>
-      <img src={img.src} alt="food" />
+      <img src={img} alt={text} />
     </div>
   );
 }
