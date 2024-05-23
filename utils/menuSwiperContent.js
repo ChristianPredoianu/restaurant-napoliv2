f9352f558c44d2282fe556cc7f5c6284f54a7c3b
Christian Predoianu <christianpredoianu@yahoo.com>
2024-05-23 15:50:24 +0200

    work

--- utils/menuSwiperContent.js
+++ utils/menuSwiperContent.js
@@ -1,21 +1,21 @@
-import PizzaImg from '@/assets/images/pizza.jpg';
-import HamburgerImg from '@/assets/images/hamburger.jpg';
-import KebabImg from '@/assets/images/kebab.jpg';
-import SaladImg from '@/assets/images/pasta.jpg';
-import PastaImg from '@/assets/images/salad.jpg';
-import SteakImg from '@/assets/images/steak.jpg';
-import PlankImg from '@/assets/images/plank.jpg';
-import PotatoeImg from '@/assets/images/potatoe.jpg';
-import FishImg from '@/assets/images/fish.jpg';
+/* import PizzaImg from '@/assets/images/pizza.jpg'; */
+/* import HamburgerImg from '@/assets/images/hamburger.jpg'; */
+/* import KebabImg from '@/assets/images/kebab.jpg'; */
+/* import SaladImg from '@/assets/images/pasta.jpg'; */
+/* import PastaImg from '@/assets/images/salad.jpg'; */
+/* import SteakImg from '@/assets/images/steak.jpg'; */
+/* import PlankImg from '@/assets/images/plank.jpg'; */
+/* import PotatoeImg from '@/assets/images/potatoe.jpg'; */
+/* import FishImg from '@/assets/images/fish.jpg'; */
 
 export const menuSwiperContent = [
-  { id: 1, img: PizzaImg, text: 'Pizza' },
-  { id: 2, img: HamburgerImg, text: 'Hamburgare' },
-  { id: 3, img: SteakImg, text: 'Kött' },
-  { id: 4, img: PastaImg, text: 'Sallad' },
-  { id: 5, img: SaladImg, text: 'Pasta' },
-  { id: 6, img: KebabImg, text: 'Kebab' },
-  { id: 7, img: PlankImg, text: 'Planka' },
-  { id: 8, img: PotatoeImg, text: 'Potatis' },
-  { id: 9, img: FishImg, text: 'Fisk' },
+  { id: 1, img: '/pizza.jpg', text: 'Pizza' },
+  { id: 2, img: '/hamburger.jpg', text: 'Hamburgare' },
+  { id: 3, img: '/steak.jpg', text: 'Kött' },
+  { id: 4, img: '/pasta.jpg', text: 'Sallad' },
+  { id: 5, img: '/pasta.jpg', text: 'Pasta' },
+  { id: 6, img: '/kebab.jpg', text: 'Kebab' },
+  { id: 7, img: '/plank.jpg', text: 'Planka' },
+  { id: 8, img: '/potatoe.jpg', text: 'Potatis' },
+  { id: 9, img: '/fish.jpg', text: 'Fisk' },
 ];
