b980a51f109e0c09309cd3ac8523a2cd0f01267e
Christian Predoianu <christianpredoianu@yahoo.com>
2024-07-03 13:32:34 +0200

    work

--- components/menu-list/MenuListItem.js
+++ components/menu-list/MenuListItem.js
@@ -1,38 +1,37 @@
 export default function MenuListItem({ data }) {
-  const { id, name, content, priceFamily, priceNormal, price, small, large } =
+  const { id, name, content, priceFamily, priceNormal, price, small, large, xLarge } =
     data;
 
   return (
-    <li className="flex flex-col mt-2 border-t-2 border-amber-600  py-6 first:border-t-0 last:border-b-2 dark:border-t last:border-amber-600">
-      <div className="flex flex-col">
-        <div className="flex items-center ">
-          <p className="text-2xl font-bold text-amber-600 ">{`${id}.`}</p>
-          <p className="font-bold text-xl md:text-2xl ml-2 ">{name}</p>
+    <li className='flex flex-col mt-2 border-t-2 border-amber-600  py-6 first:border-t-0 last:border-b-2 dark:border-t last:border-amber-600'>
+      <div className='flex flex-col'>
+        <div className='flex items-center '>
+          <p className='text-2xl font-bold text-amber-600 '>{`${id}.`}</p>
+          <p className='font-bold text-xl md:text-2xl ml-2 '>{name}</p>
         </div>
-        <div className="flex items-center justify-between">
-          <p className="py-2 text-md sm:text-xl pr-4">{content}</p>
+        <div className='flex items-center justify-between'>
+          <p className='py-2 text-md sm:text-xl pr-4'>{content}</p>
           {price && (
-            <p className="text-red-500 dark:text-amber-600 text-2xl">{`${price}:-`}</p>
+            <p className='text-red-500 dark:text-amber-600 text-2xl'>{`${price}:-`}</p>
           )}
         </div>
         {small && large && (
-          <div className="text-red-500 dark:text-amber-600 text-xl md:text-2xl">
+          <div className='text-red-500 dark:text-amber-600 text-xl md:text-2xl'>
             <p>{`${small} :-`}</p>
-            <p className="mt-2">{`${large} :- `}</p>
+            <p className='mt-2'>{`${large} :- `}</p>
+            {xLarge && <p className='mt-2'>{`${xLarge} :- `}</p>}
           </div>
         )}
         {priceNormal && (
-          <div className="flex flex-col md:flex-row mt-2 text-xl">
-            <p className="text-red-500 dark:text-amber-600">
-              <span className="text-black dark:text-gray-200 mr-2 ">
-                Vanlig:
-              </span>
+          <div className='flex flex-col md:flex-row mt-2 text-xl'>
+            <p className='text-red-500 dark:text-amber-600'>
+              <span className='text-black dark:text-gray-200 mr-2 '>Vanlig:</span>
               {`${priceNormal} :-`}
             </p>
             {priceFamily && (
-              <p className="md:ml-2 ">
+              <p className='md:ml-2 '>
                 Familje:
-                <span className=" md:m-2  ml-2 text-red-500 dark:text-amber-600">{`${priceFamily} :-`}</span>
+                <span className=' md:m-2  ml-2 text-red-500 dark:text-amber-600'>{`${priceFamily} :-`}</span>
               </p>
             )}
           </div>
