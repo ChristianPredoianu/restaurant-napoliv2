3a3cd2ac0f43a9b25f66c625e1dba490dbca7aa1
Christian Predoianu <christianpredoianu@yahoo.com>
2024-07-03 13:32:42 +0200

    work

--- components/lunch-list/LunchListItem.js
+++ components/lunch-list/LunchListItem.js
@@ -1,7 +1,7 @@
 export default function LunchListItem({ day, lunch }) {
   return (
-    <li className="py-2 border-b border-amber-600">
-      <p className="text-xl">{day}</p>
+    <li className='py-2 border-b  border-amber-600 lg:w-1/2'>
+      <p className='text-xl'>{day}</p>
       <p>{lunch}</p>
     </li>
   );
