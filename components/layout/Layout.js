15cf2237303e7176047ebeaea0201f1a421dcd68
Christian Predoianu <christianpredoianu@yahoo.com>
2026-05-28 17:45:54 +0200

    redisign layout

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,13 +1,20 @@
 import Nav from '@/components/nav/Nav';
 import ScrollToTop from '@/components/ui/ScrollToTop';
+import GlobalBackground from '@/components/layout/GlobalBackground';
 
 export default function Layout({ children }) {
   return (
-    <>
+    <div className='relative min-h-screen text-zinc-900 dark:text-gray-200'>
+      {/* GLOBAL BACKGROUND (MUST BE HERE) */}
+      <GlobalBackground />
+
+      {/* NAV */}
       <Nav />
-      <main>{children}</main>
+
+      {/* CONTENT */}
+      <main className='relative z-10'>{children}</main>
 
       <ScrollToTop />
-    </>
+    </div>
   );
 }
