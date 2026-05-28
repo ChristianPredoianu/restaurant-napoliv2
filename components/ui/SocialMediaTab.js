35d1602193ec902e34a104a97c710186687a8858
Christian Predoianu <christianpredoianu@yahoo.com>
2026-05-28 17:50:11 +0200

    redesign socialmedia tab

--- components/ui/SocialMediaTab.js
+++ components/ui/SocialMediaTab.js
@@ -3,43 +3,93 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebook, faInstagram } from '@fortawesome/free-brands-svg-icons';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
-export default forwardRef(function SocialMediaTab({ props }, ref) {
+const brandRed = '#991b1b';
+
+export default forwardRef(function SocialMediaTab(props, ref) {
   return (
     <div
-      className="flex flex-col gap-y-4 items-center bg-amber-600 text-white text-2xl py-4 px-2 rounded-l-lg shadow-xl"
       ref={ref}
+      className='
+        flex flex-col items-center gap-5
+
+        px-3 py-5
+
+        rounded-l-2xl
+
+        border border-white/10
+        bg-white/5
+        backdrop-blur-xl
+
+        shadow-[0_20px_60px_rgba(0,0,0,0.4)]
+      '
     >
+      {/* Facebook */}
       <a
-        href="https://www.facebook.com/NapoliRestaurang"
-        rel="noreferrer nofollow"
-        target="_blank"
-        className="hover:scale-105 hover:text-black"
+        href='https://www.facebook.com/NapoliRestaurang'
+        target='_blank'
+        rel='noreferrer nofollow'
+        className='group transition-transform hover:-translate-y-1'
       >
         <FontAwesomeIcon
           icon={faFacebook}
-          className="cursor-pointer transition duration-150 ease-in-out"
+          className='
+            text-xl
+            text-zinc-300
+            group-hover:text-white
+            transition
+          '
+          style={{
+            filter: 'drop-shadow(0 0 10px rgba(153,27,27,0.35))',
+          }}
         />
       </a>
+
+      {/* Instagram */}
       <a
-        href="https://www.instagram.com/napoli_restaurang/"
-        rel="noreferrer nofollow"
-        target="_blank"
-        className="hover:scale-105 hover:text-black"
+        href='https://www.instagram.com/napoli_restaurang/'
+        target='_blank'
+        rel='noreferrer nofollow'
+        className='group transition-transform hover:-translate-y-1'
       >
         <FontAwesomeIcon
           icon={faInstagram}
-          className="transition duration-150 ease-in-out cursor-pointer"
+          className='
+            text-xl
+            text-zinc-300
+            group-hover:text-white
+            transition
+          '
+          style={{
+            filter: 'drop-shadow(0 0 10px rgba(153,27,27,0.35))',
+          }}
         />
       </a>
+
+      {/* Email */}
       <a
-        href="mailto:musti_076@napoli.com"
-        className="hover:scale-105 hover:text-black"
+        href='mailto:musti_076@napoli.com'
+        className='group transition-transform hover:-translate-y-1'
       >
         <FontAwesomeIcon
           icon={faEnvelope}
-          className="transition duration-150 ease-in-out cursor-pointer"
+          className='
+            text-xl
+            text-zinc-300
+            group-hover:text-white
+            transition
+          '
+          style={{
+            color: brandRed,
+            filter: 'drop-shadow(0 0 12px rgba(153,27,27,0.5))',
+          }}
         />
       </a>
+
+      {/* Accent line */}
+      <div
+        className='w-[1px] h-10 mt-2 opacity-40'
+        style={{ backgroundColor: brandRed }}
+      />
     </div>
   );
 });
