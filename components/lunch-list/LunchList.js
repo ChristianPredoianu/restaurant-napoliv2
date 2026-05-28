f8e0ceec5550484a0f54ab7cad6ef442c3eb134a
Christian Predoianu <christianpredoianu@yahoo.com>
2026-05-28 17:46:13 +0200

    redisign lunchlist

--- components/lunch-list/LunchList.js
+++ components/lunch-list/LunchList.js
@@ -2,7 +2,7 @@ import LunchListItem from '@/components/lunch-list/LunchListItem';
 
 export default function LunchList({ lunchData }) {
   return (
-    <ul>
+    <ul className='grid gap-4 sm:grid-cols-2 lg:grid-cols-3'>
       {Object.entries(lunchData).map(([key, value]) => (
         <LunchListItem key={key} day={value.day} lunch={value.lunch} />
       ))}
