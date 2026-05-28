447a0c8b3d70da7b3f915e3a3eea7a3b2b901276
Christian Predoianu <christianpredoianu@yahoo.com>
2026-05-28 17:50:46 +0200

    redesign verticalswiper

--- components/vertical-swiper/VerticalSwiper.js
+++ components/vertical-swiper/VerticalSwiper.js
@@ -1,15 +1,18 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/pagination';
+
 import { verticalSwiperImages } from '@/utils/verticalSwiperImages';
 import SwiperContent from '@/components/vertical-swiper/SwiperContent';
+
 import { Autoplay, Pagination } from 'swiper';
 
+const brandRed = '#991b1b';
 export default function VerticalSwiper() {
   return (
-    <div className="swiper-container">
+    <div className='relative h-[28rem] md:h-[32rem] w-full'>
       <Swiper
-        direction={'vertical'}
+        direction='vertical'
         autoplay={{
           delay: 3500,
           disableOnInteraction: false,
@@ -18,11 +21,11 @@ export default function VerticalSwiper() {
           clickable: true,
         }}
         modules={[Autoplay, Pagination]}
-        className="mySwiper"
+        className='h-full w-full rounded-3xl overflow-hidden'
       >
         {verticalSwiperImages.map((item) => (
-          <SwiperSlide key={item.id}>
-            {<SwiperContent img={item.img} />}
+          <SwiperSlide key={item.id} className='!h-full !w-full'>
+            <SwiperContent img={item.img} />
           </SwiperSlide>
         ))}
       </Swiper>
