2b00ee1055074e0cbfd53cee0bfd835992de1414
Christian Predoianu <christianpredoianu@yahoo.com>
2026-05-28 17:47:55 +0200

    redisign hamburger

--- components/nav/Hamburger.js
+++ components/nav/Hamburger.js
@@ -1,33 +1,60 @@
-import classNames from 'classnames';
-
 export default function Hamburger({ isNavbarOpen, onToggleNavbar }) {
-  const genericHamburgerLine = `h-1 w-10 my-1 rounded-full bg-black transition ease transform duration-300 bg-black dark:bg-amber-600`;
   return (
-    <>
-      <button
-        className={classNames(
-          'flex flex-col h-12 w-12 justify-center items-center z-50 group lg:hidden',
-          { 'fixed top-6 right-4': isNavbarOpen }
-        )}
-        type="button"
-        onClick={onToggleNavbar}
-      >
-        <div
-          className={classNames(genericHamburgerLine, {
-            'rotate-45 translate-y-3 dark:bg-white': isNavbarOpen,
-          })}
+    <button
+      onClick={onToggleNavbar}
+      aria-label='Toggle menu'
+      className={`
+        relative z-50 lg:hidden
+        flex flex-col items-center justify-center
+        w-12 h-12
+        rounded-xl
+        transition-all duration-300 ease-out
+        hover:bg-black/5 dark:hover:bg-white/10
+        active:scale-95
+      `}
+    >
+      {/* TOP LINE */}
+      <span
+        className={`
+          block h-[2px] w-7 rounded-full
+          transition-all duration-300 ease-in-out
+          bg-red-800 dark:bg-red-500
+
+          ${isNavbarOpen ? 'rotate-45 translate-y-[6px]' : '-translate-y-[6px]'}
+        `}
+      />
+
+      {/* MIDDLE LINE */}
+      <span
+        className={`
+          block h-[2px] w-7 rounded-full
+          transition-all duration-300 ease-in-out
+          bg-red-800 dark:bg-red-500
+
+          ${isNavbarOpen ? 'opacity-0 scale-75' : 'opacity-100 scale-100'}
+        `}
+      />
+
+      {/* BOTTOM LINE */}
+      <span
+        className={`
+          block h-[2px] w-7 rounded-full
+          transition-all duration-300 ease-in-out
+          bg-red-800 dark:bg-red-500
+
+          ${isNavbarOpen ? '-rotate-45 -translate-y-[6px]' : 'translate-y-[6px]'}
+        `}
+      />
+
+      {/* glow when open */}
+      {isNavbarOpen && (
+        <span
+          className='
+          absolute inset-0 rounded-xl
+          shadow-[0_0_20px_rgba(153,27,27,0.25)]
+        '
         />
-        <div
-          className={classNames(genericHamburgerLine, {
-            'opacity-0 ': isNavbarOpen,
-          })}
-        />
-        <div
-          className={classNames(genericHamburgerLine, {
-            '-rotate-45 -translate-y-3 dark:bg-white': isNavbarOpen,
-          })}
-        />
-      </button>
-    </>
+      )}
+    </button>
   );
 }
