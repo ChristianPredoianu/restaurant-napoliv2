2e17a236d37fe2da524dfa57bfd84ba197ba4cb8
Christian Predoianu <christianpredoianu@yahoo.com>
2026-05-28 17:47:10 +0200

    redisign menulswiper

--- components/menu-swiper/MenuSwiper.js
+++ components/menu-swiper/MenuSwiper.js
@@ -2,6 +2,7 @@ import { forwardRef } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { menuSwiperContent } from '@/utils/menuSwiperContent';
 import MenuSwiperContent from '@/components/menu-swiper/MenuSwiperContent';
+
 import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
@@ -10,40 +11,33 @@ import { Autoplay, Pagination, Navigation } from 'swiper';
 
 export default forwardRef(function MenuSwiper({ onScrollToSection }, ref) {
   return (
-    <div className='swiper-container' ref={ref}>
-      <p className='text-center pb-8'>
-        Klicka på önskad kategori för att komma direkt till menyn
-      </p>
+    <div ref={ref} className='w-full'>
+      {/* HEADER TEXT */}
+      <div className='text-center mb-8'>
+        <p className='text-sm md:text-base text-zinc-500 dark:text-zinc-400 tracking-wide'>
+          Välj en kategori för att hoppa direkt till menyn
+        </p>
+      </div>
+
       <Swiper
-        navigation={true}
+        navigation
         slidesPerView={1}
-        spaceBetween={30}
-        pagination={{
-          clickable: true,
-        }}
-        breakpoints={{
-          640: {
-            slidesPerView: 2,
-            spaceBetween: 20,
-          },
-          1024: {
-            slidesPerView: 3,
-            spaceBetween: 50,
-          },
-        }}
+        spaceBetween={20}
+        pagination={{ clickable: true }}
         autoplay={{
           delay: 3500,
           disableOnInteraction: false,
         }}
+        breakpoints={{
+          640: { slidesPerView: 2, spaceBetween: 16 },
+          1024: { slidesPerView: 3, spaceBetween: 24 },
+        }}
         modules={[Autoplay, Pagination, Navigation]}
-        className='mySwiper'
+        className='menuSwiper'
       >
         {menuSwiperContent.map((item) => (
           <SwiperSlide key={item.id}>
-            <MenuSwiperContent
-              item={item}
-              onScrollToSection={onScrollToSection}
-            />
+            <MenuSwiperContent item={item} onScrollToSection={onScrollToSection} />
           </SwiperSlide>
         ))}
       </Swiper>
