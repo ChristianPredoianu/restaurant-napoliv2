cdd55fe2cf94918a2316d17b9daa12bb336eb241
Christian Predoianu <christianpredoianu@yahoo.com>
2026-05-28 17:48:37 +0200

    redisign navlogo

--- components/nav/NavLogo.js
+++ components/nav/NavLogo.js
@@ -1,12 +1,28 @@
 import Link from 'next/link';
-import Logo from '@/assets/images/napoli-logo.png';
 
 export default function NavLogo() {
   return (
-    <Link href="/">
-      <a className="text-black font-bold dark:text-gray-300 md:mr-4 w-16 sm:w-20 md:w-24">
-        <img src={Logo.src} alt="Logo" />
-      </a>
+    <Link
+      href='/'
+      className='
+        flex flex-col
+        transition-transform duration-300
+        hover:scale-105
+        outline-none
+        select-none
+      '
+      aria-label='Napoli Home'
+    >
+      <div className='cursor-pointer'>
+        {' '}
+        {/* Add a wrapper */}
+        <span className='font-sans font-black text-xl sm:text-2xl md:text-3xl tracking-wider text-red-800 uppercase leading-none'>
+          Napoli
+        </span>
+        <span className='font-sans font-bold text-[9px] sm:text-[10px] md:text-xs tracking-[0.22em] text-zinc-500 uppercase mt-0.5 leading-none'>
+          Restaurang
+        </span>
+      </div>
     </Link>
   );
 }
