bf4efa63c9774cabbcdb1ab6950012e79d723f56
Christian Predoianu <christianpredoianu@yahoo.com>
2026-05-28 17:46:40 +0200

    redisign menulist

--- components/menu-list/MenuList.js
+++ components/menu-list/MenuList.js
@@ -2,18 +2,49 @@ import { forwardRef } from 'react';
 import MenuListItem from '@/components/menu-list/MenuListItem';
 
 export default forwardRef(function MenuList({ data, heading, category }, ref) {
+  // Normalize object → array safely
+  const items = Array.isArray(data) ? data : Object.values(data || {}).flat();
+
   return (
-    <div className="flex flex-col" ref={ref}>
-      <h3 className="text-2xl md:text-3xl mt-10 uppercase text-center text-red-700 dark:text-amber-600">
+    <div
+      ref={ref}
+      className='
+        w-full
+        flex flex-col
+        py-10
+      '
+    >
+      {/* CATEGORY TITLE */}
+      <h3
+        className='
+          text-3xl md:text-4xl
+          font-black
+          uppercase
+          text-center
+          tracking-widest
+          text-zinc-900 dark:text-red-900
+        '
+      >
         {category}
       </h3>
-      <h4 className="mt-4 text-amber-700 dark:text-gray-400 font-bold text-md md:text-xl text-center">
+
+      {/* SUBTITLE */}
+      <h4
+        className='
+          mt-3
+          text-sm md:text-lg
+          text-center
+          text-zinc-500 dark:text-zinc-400
+          font-medium
+        '
+      >
         {heading}
       </h4>
 
-      <ul className="mt-4">
-        {data.map((menuItem) => (
-          <MenuListItem key={menuItem.id} data={menuItem} />
+      {/* LIST */}
+      <ul className='mt-10 space-y-3'>
+        {items.map((menuItem) => (
+          <MenuListItem key={`${menuItem.id}-${menuItem.name}`} data={menuItem} />
         ))}
       </ul>
     </div>
