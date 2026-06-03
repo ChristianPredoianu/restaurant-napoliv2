fa2c502f29de17899f4c82b9a9fb7582661189a2
Christian Predoianu <christianpredoianu@yahoo.com>
2026-06-03 09:58:49 +0200

    changed padding top

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -6,7 +6,7 @@ export default function Layout({ children }) {
     <div className='relative min-h-screen text-zinc-900  dark:bg-dark-mode-blue dark:text-gray-200'>
       <Nav />
 
-      <main className='relative z-10 pt-24'>
+      <main className='relative z-10 pt-12 md:pt-16 lg:pt-20'>
         <div className=''>{children}</div>
       </main>
 
